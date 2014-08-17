efbaec690fe0c2acc33adc6cddb3c5509c3eb3d3
Jan Schulte <hello@unexpected-co.de>
2014-08-17 21:46:43 +0200

    authenticate with 2-fa token

--- lib/monotes/authenticator.rb
+++ lib/monotes/authenticator.rb
@@ -8,9 +8,11 @@ module Monotes
     def get_oauth_token(username, password, &acquire_two_fa)
       api_client = @api_client_block.call(:login => username, :password => password)
       begin
-      api_client.create_authorization(:scopes => ["user"], :note => ACCESS_NOTE)
+        api_client.create_authorization(:scopes => ["user"], :note => ACCESS_NOTE)
       rescue Octokit::OneTimePasswordRequired => otp_error
         two_fa_token = yield acquire_two_fa
+        api_client.create_authorization(:scopes => ["user"], :note => ACCESS_NOTE,
+                                       :headers => { "X-GitHub-OTP" => two_fa_token })
       end
     end
   end
--- spec/unit/authenticator_spec.rb
+++ spec/unit/authenticator_spec.rb
@@ -6,32 +6,40 @@ describe Monotes::Authenticator do
   let(:api_client_mock) { double('Octokit::Client') }
   let(:username) { 'Jim' }
   let(:password) { 'passw' }
-  let(:expected_token) { 'expected_token' }
+  let(:expected_oauth_token) { 'expected_oauth_token' }
 
   context 'without 2FA' do
     subject(:authenticator) { Monotes::Authenticator.new { |user, pass| api_client_mock } }
 
     before do
-      allow(api_client_mock).to receive(:create_authorization).with(any_args).and_return(expected_token)
+      allow(api_client_mock).to receive(:create_authorization).with(any_args).and_return(expected_oauth_token)
     end
 
     it 'authenticates without asking for 2-FA token' do
       actual_token = authenticator.get_oauth_token(username, password) { raise "2-FA Block was called" }
-      expect(actual_token).to eq expected_token
+      expect(actual_token).to eq expected_oauth_token
     end
   end
   context 'with 2FA' do
+    let(:two_fa_token) { '2-factor token' }
     subject(:authenticator) { Monotes::Authenticator.new { |user, pass| api_client_mock } }
 
     before do
       params = { :scopes => ["user"], :note => Monotes::Authenticator::ACCESS_NOTE }
+      params_with_2fa = params.merge(:headers => { "X-GitHub-OTP" => two_fa_token })
       allow(api_client_mock).to receive(:create_authorization).with(params).and_raise(Octokit::OneTimePasswordRequired)
+      allow(api_client_mock).to receive(:create_authorization).with(params_with_2fa).and_return(expected_oauth_token)
     end
 
     it 'authenticates and asks for 2-FA token' do
       block_called = false
-      authenticator.get_oauth_token(username, password) { block_called = true }
+      authenticator.get_oauth_token(username, password) { block_called = true; two_fa_token }
       expect(block_called).to be true
+    end
+
+    it 'authenticates with 2-FA token' do
+      actual_token = authenticator.get_oauth_token(username, password) { two_fa_token }
+      expect(actual_token).to eq expected_oauth_token
     end
   end
 end

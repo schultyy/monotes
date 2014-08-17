65847dc51247d44e9ee0bb420f42cdb00bd6084c
Jan Schulte <hello@unexpected-co.de>
2014-08-17 22:01:38 +0200

    Ask for Credentials

--- lib/monotes/cli/application.rb
+++ lib/monotes/cli/application.rb
@@ -1,4 +1,6 @@
 require 'thor'
+require 'octokit'
+require 'monotes/authenticator'
 
 module Monotes
   module CLI
@@ -7,9 +9,15 @@ module Monotes
       def login
         print "Username > "
         username = STDIN.gets.chomp
-        print("Password > ")
+        print "Password > "
         password = STDIN.noecho(&:gets).chomp
-
+        puts "\n"
+        authenticator = Monotes::Authenticator.new(Octokit::Client)
+        oauth_token = authenticator.get_oauth_token(username, password) do
+          print "Two-Factor token > "
+          STDIN.gets.chomp
+        end
+        puts oauth_token.inspect
       end
     end
   end

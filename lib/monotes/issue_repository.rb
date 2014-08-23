797fe5d41fa80f5dac2ed1331cc625f0aad5d10e
Jan Schulte <hello@unexpected-co.de>
2014-08-23 14:00:53 +0200

    Rename fs/delegate to context

--- lib/monotes/issue_repository.rb
+++ lib/monotes/issue_repository.rb
@@ -4,14 +4,14 @@ module Monotes
   class IssueRepository
 
     def initialize(args)
-      @fs = args.fetch(:fs)
+      @context = args.fetch(:context)
       @repository = args.fetch(:repository)
     end
 
     def save(args)
       issues = Array(args)
       issues.each do |issue|
-        @fs.save(issue.to_yaml)
+        @context.save(issue.to_yaml)
       end
     end
   end
--- spec/unit/issue_repository_spec.rb
+++ spec/unit/issue_repository_spec.rb
@@ -4,38 +4,38 @@ require 'monotes/issue_repository'
 
 describe Monotes::IssueRepository do
   let(:issues) { build_list(:issue, 2) }
-  let(:delegate) { double('fs delegate') }
+  let(:context) { double('fs context') }
   let(:repository_name) { 'franz/franz-seins' }
 
   context '#initialize' do
-    it 'accepts delegate and repository' do
-      expect { Monotes::IssueRepository.new(fs: delegate, repository: repository_name) }.to_not raise_error
+    it 'accepts context and repository' do
+      expect { Monotes::IssueRepository.new(context: context, repository: repository_name) }.to_not raise_error
     end
 
-    it 'raises error when delegate not passed' do
+    it 'raises error when context not passed' do
       expect { Monotes::IssueRepository.new(repository: repository_name) }.to raise_error
     end
 
     it 'raises error when repository not passed' do
-      expect { Monotes::IssueRepository.new(fs: delegate) }.to raise_error
+      expect { Monotes::IssueRepository.new(context: context) }.to raise_error
     end
   end
 
   context '#save' do
-    let(:repository) { Monotes::IssueRepository.new(repository: repository_name, fs: delegate) }
+    let(:repository) { Monotes::IssueRepository.new(repository: repository_name, context: context) }
     let(:issue) { issues.first }
     before do
-      allow(delegate).to receive(:save)
+      allow(context).to receive(:save)
     end
 
     it 'saves a single issue' do
       repository.save(issue)
-      expect(delegate).to have_received(:save).with(issue.to_yaml)
+      expect(context).to have_received(:save).with(issue.to_yaml)
     end
 
     it 'saves a list of issues' do
       repository.save(issues)
-      expect(delegate).to have_received(:save).twice
+      expect(context).to have_received(:save).twice
     end
   end
 end
